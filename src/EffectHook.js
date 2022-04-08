0ea9c6f6379d8ddd28ec01310c0db1b21476f49d
louischin <z23320785@gmail.com>
2022-04-09 00:11:45 +0800

    add

--- src/EffectHook.js
+++ src/EffectHook.js
@@ -1,6 +1,6 @@
 
 
-import React, {useCallback} from 'react';
+import React, {useCallback,useState, useEffect} from 'react';
 
 
 class EffectHook extends React.Component {
@@ -10,10 +10,15 @@ class EffectHook extends React.Component {
     this.state = {
       count: 0
     };
+    this.Example();
     this.setState({ count: 5 });
     console.log('%c this.state','background: red; color: white',this.state);
   }
   //加入組件建構完成後執行的事件
+
+   
+
+
   componentDidMount() {//Constructor
     document.title = `You clicked ${this.state.count} times`;
     console.log('%c componentDidMount','background: red; color: white',this.state.count,this);
@@ -24,7 +29,6 @@ class EffectHook extends React.Component {
     console.log('%c componentDidUpdate','background: blue; color: white',this.state.count,this);
 
   }
-
   render() {
     return (
       <div>
@@ -35,6 +39,28 @@ class EffectHook extends React.Component {
       </div>
     );
   }
+
+  
 }
+function Example() {
+  const [count, setCount] = useState(0);
+
+  // 相似於 componentDidMount 和 componentDidUpdate:
+  useEffect(() => {
+    // 使用瀏覽器 API 更新文件標題
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+
 
 export default EffectHook;
