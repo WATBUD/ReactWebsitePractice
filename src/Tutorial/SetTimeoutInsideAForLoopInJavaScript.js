145989bee0fa0054a590ed4f90523a85c55f2631
水靈 <z23320785@gmail.com>
2023-11-09 17:29:46 +0800

    Organizing code

--- src/Tutorial/SetTimeoutInsideAForLoopInJavaScript.js
+++ src/Tutorial/SetTimeoutInsideAForLoopInJavaScript.js
@@ -1,94 +1,14 @@
 /* eslint-disable no-loop-func */
-import React from 'react';
-function SetTimeoutInsideAForLoopInJavaScript(type='') {
-  switch (type) {
-    case "let":
-      //藉助let 的作用域 5 -> 0,1,2,3,4
-      for (let i = 0; i < 5; i++) {
-        // Delay each iteration by 1 second
-        setTimeout(
-          function () {
-            console.log(i);
-          },
-          1000 * i,
-          i
-        );
-      }
-      break;
-    case "var":
-      //opuput 5 -> 5,5,5,5,5
-      for (var i = 0; i < 5; i++) {
-        // Delay each iteration by 1 second
-        setTimeout(
-          function () {
-            console.log(i);
-          },
-          1000 * i,
-          i
-        );
-      }
-      break;
-    case "IIFE":
-      //immediately-invoked function expression
-      for (var i_iife = 0; i_iife < 5; i_iife++) {
-        ((j) => {
-          setTimeout(() => {
-            console.log(j);
-          }, 1000);
-        })(i_iife);
-      }
-      console.log('%c Enter IIFE', 
-      'background-image: url(https://pic.pimg.tw/elizabeth15angel/1437747627-1122098840.jpg); background-size: 100% 100%;color:yellow;font-size: 40px;'
-      ,'i_iife',i_iife);
-      break;
-    case "Promise":
-      const task = [];
-      const output = (i) =>
-        new Promise((resolve) => {
-          setTimeout(() => {
-            console.log(i);
-            resolve();
-          }, 1000 * i);
-        });
-
-      for (var i_Promise = 0; i_Promise < 5; i_Promise++) {
-        task.push(output(i_Promise));
-      }
-
-      // 4.异步操作执行完成后输出最后的i
-      Promise.all(task).then(() => {
-        setTimeout(() => {
-          console.log(i_Promise);
-        }, 1000);
-      });
-      break;
-
-    // case "async/await":
-    //   const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-
-    //   (async () => {
-    //       for(var i = 0; i < 5; i++) {
-    //           if (i > 0) {
-    //               await sleep(1000)
-    //           }
-    //           console.log(i)
-    //       }
-    //       await sleep(1000)
-    //       console.log(i)
-    //   })()
-    //   break;
-
-    default:
-      break;
-  }
-
-}
+import React, { Fragment } from 'react';
+import { useState,useCallback,useEffect } from 'react';
 
 
 
 
-function customName() {
-  let type="var";
+
+
+function CustomName() {
+  //let type="let";
   let questionText = `1. for (var i = 0; i < 5; i++) {
     setTimeout(() => { 
         console.log(i); 
@@ -97,20 +17,149 @@ function customName() {
   a. 為什麼結果會顯示5顯示五次？
   b. 為什麼結果是一次顯示，而非間隔一秒地顯示？
   c. 如何修改能間隔一秒，每秒顯示的是該次的i？`;
+  const [resultText, setResultText] = useState("none");
+  const [isRunning, setIsRunning] = useState(false); // 用于追踪进程是否正在运行
+
+
+  const setTimeoutInsideAForLoopInJavaScript = (type='') => {
+    if (isRunning) {
+      console.log('Already running.');
+      return;
+    }
+    setIsRunning(true);
+
+    switch (type) {
+      case "let":
+        //藉助let 的作用域 5 -> 0,1,2,3,4
+        for (let i = 0; i < 5; i++) {
+          // Delay each iteration by 1 second
+          setTimeout(
+            function () {
+              console.log(i);
+              setResultText(i);
+            },
+            1000 * i,
+            i
+          );
+        }
+        break;
+      case "var":
+        //opuput 5 -> 5,5,5,5,5
+        for (var i = 0; i < 5; i++) {
+          // Delay each iteration by 1 second
+          setTimeout(
+            function () {
+              console.log(i);
+              setResultText(i);
+            },
+            1000 * i,
+            i
+          );
+        }
+        break;
+      case "IIFE":
+        //immediately-invoked function expression
+        for (var i_iife = 0; i_iife < 5; i_iife++) {
+          ((j) => {
+            setTimeout(() => {
+              console.log(j);
+              setResultText(j);
+            }, 1000);
+          })(i_iife);
+        }
+
+        console.log('%c Enter IIFE', 
+        'background-image: url(https://pic.pimg.tw/elizabeth15angel/1437747627-1122098840.jpg); background-size: 100% 100%;color:yellow;font-size: 40px;'
+        ,'i_iife',i_iife);
+        break;
+      case "Promise":
+        const task = [];
+        const output = (i) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              console.log(i);
+              setResultText(i);
+              resolve();
+            }, 1000 * i);
+          });
+  
+        for (var i_Promise = 0; i_Promise < 5; i_Promise++) {
+          task.push(output(i_Promise));
+        }
+  
+        // 4.异步操作执行完成后输出最后的i
+        Promise.all(task).then(() => {
+          setTimeout(() => {
+            console.log(i_Promise);
+            setResultText(i);
+          }, 1000);
+        });
+        break;
+  
+      // case "async/await":
+      //   const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+  
+      //   (async () => {
+      //       for(var i = 0; i < 5; i++) {
+      //           if (i > 0) {
+      //               await sleep(1000)
+      //           }
+      //           console.log(i)
+      //       }
+      //       await sleep(1000)
+      //       console.log(i)
+      //   })()
+      //   break;
+  
+      default:
+        break;
+    }
+
+    setTimeout(() => {
+      setIsRunning(false); 
+    }, 5000);  
+
+  }; // useCallback 的依赖列表为空，意味着函数只在组件首次渲染时创建一次
+
+
+  
+  // useEffect(() => {
+  //   // 假设您只想在组件加载时运行此函数
+  //   setTimeoutInsideAForLoopInJavaScript(type);
+  // }, [setTimeoutInsideAForLoopInJavaScript, type]);
+
+
+
 
   return (
     <div>
       <textarea id="questionText" rows="10" cols="50" value={questionText} readOnly />
 
-      {/* <p>Run SetTimeoutInsideAForLoopInJavaScript,var= {SetTimeoutInsideAForLoopInJavaScript("var")}</p>
-      <p>Run SetTimeoutInsideAForLoopInJavaScript,let= {SetTimeoutInsideAForLoopInJavaScript("let")}</p> */}
+      {!isRunning &&(
+        <Fragment>
+      <button onClick={() => setTimeoutInsideAForLoopInJavaScript('let')}>
+        Run with let
+      </button>
+      <button onClick={() => setTimeoutInsideAForLoopInJavaScript('var')}>
+        Run with var
+      </button>
+      <button onClick={() => setTimeoutInsideAForLoopInJavaScript('IIFE')}>
+        Run with IIFE
+      </button>
+      <button onClick={() => setTimeoutInsideAForLoopInJavaScript('Promise')}>
+        Run with Promise
+      </button>
+      </Fragment>)}
 
-      <p>Run SetTimeoutInsideAForLoopInJavaScript,{type} {SetTimeoutInsideAForLoopInJavaScript(type)}</p>
 
+      <p>Time: {resultText}</p>
+
+      {/* <p>Run SetTimeoutInsideAForLoopInJavaScript,{type}=</p>
+      <p>time:{resultText}</p> */}
 
     </div>
     
   );
 }
-export default customName;
+export default CustomName;
 
